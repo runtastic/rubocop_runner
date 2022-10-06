4bfc1e45c6535179472c8bfb90267389b9368378
Dominik Goltermann <dominik.goltermann@runtastic.com>
2022-10-06 13:59:39 +0200

    CAB-1017 Gemfile.lock is not touched by running this anymore
    Also it executes a lot faster as it does not need to require the whole
    bundle

--- lib/rubocop_runner.rb
+++ lib/rubocop_runner.rb
@@ -6,81 +6,16 @@ require 'rubocop'
 module RubocopRunner
   module_function
 
-  # from https://github.com/djberg96/ptools/blob/master/lib/ptools.rb#L90
-  def binary?(file)
-    return true if File.ftype(file) != 'file'
-
-    s = (File.read(file, File.stat(file).blksize) || '').split(//)
-    ((s.size - s.grep(' '..'~').size) / s.size.to_f) > 0.30
-  end
-
-  def staged_files
-    files = `git diff --cached --name-only --diff-filter=ACM`.split
-    files.reject do |f|
-      if File.ftype(f) != 'file'
-        true
-      else
-        size = File.size(f)
-        size > 1_000_000 || (size > 20 && binary?(f))
-      end
-    end
-  end
-
-  def merge?
-    File.exist?('.git/MERGE_MSG') && File.exist?('.git/MERGE_HEAD')
-  end
-
-  def conflict_files
-    IO.read('.git/MERGE_MSG')
-      .each_line
-      .select { |e| e.start_with?("\t") }
-      .map(&:strip)
-  end
-
-  def files
-    if merge?
-      conflict_files
-    else
-      staged_files
-    end
-  end
-
-  RUBY_PATTERN = /\.(rb|gemspec|rake)$/.freeze
-  RUBY_NAMES = %w[Guardfile Gemfile Rakefile config.ru].freeze
-
-  def ruby_file?(filename)
-    RUBY_NAMES.include?(filename) || !(filename =~ RUBY_PATTERN).nil?
-  end
-
-  def staged_ruby_files
-    @ruby_files ||= files.select do |file|
-      !File.directory?(file) && File.exist?(file) && ruby_file?(file)
-    end
-  end
-
-  DEFAULT_ARGS = %w[--auto-correct
-                    --format fuubar
-                    --force-exclusion
-                    --fail-level autocorrect].freeze
-  def run
-    return 0 if staged_ruby_files.empty?
-
-    ::RuboCop::CLI.new.run(DEFAULT_ARGS + staged_ruby_files)
-  end
-
-  RUBOCOP_RUNNER_HOME =
-    Pathname.new(File.join(File.dirname(__FILE__), '..')).realpath
-
-  def root
-    RUBOCOP_RUNNER_HOME
-  end
-
   def create_backup(pre_commit_path)
     return unless File.exist?(pre_commit_path)
 
     FileUtils.mv(pre_commit_path,
-                 pre_commit_path.join('.bkp'),
+                 "#{pre_commit_path}.bak",
                  force: true)
+  end
+  
+  def root
+    Pathname.new(File.join(File.dirname(__FILE__), '..')).realpath
   end
 
   def install(root = '.')
